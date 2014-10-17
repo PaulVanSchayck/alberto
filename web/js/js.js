8b4b9389155999cb5d1a4083fae7469ec5fd88d6
PaulVanSchayck <polleke@gmail.com>
2014-10-17 16:07:59 +0200

    Update lg-svg to include all cell types

--- web/js/js.js
+++ web/js/js.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $('#change').click(function() {
         $('#lg').find('.suspensor').attr('fill','red')
+        $('#lg').find('.vascular-initials').attr('fill','green')
+        $('#lg').find('.vascular').attr('fill','blue')
     })
 });
