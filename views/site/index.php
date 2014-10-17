64dfede1ad687815b3f542dc64d70a5cab36c142
PaulVanSchayck <paul@vanschayck.nl>
2014-10-17 11:01:39 +0200

    Update some of the basic information

--- views/site/index.php
+++ views/site/index.php
@@ -1,15 +1,14 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+$this->title = 'AlBERTO';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>AlBERTO</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">AraBidopsis EmbRyonic Transcriptome brOwser</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
     <div class="body-content">
