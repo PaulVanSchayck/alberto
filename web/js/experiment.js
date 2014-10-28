528617f8e939a1e60683d3bfc6e637945ddc729d
PaulVanSchayck <polleke@gmail.com>
2014-10-28 11:12:39 +0100

    Initial work on displaying data from the table in the image

--- web/js/experiment.js
+++ web/js/experiment.js
@@ -39,16 +39,12 @@ function loadExperiment() {
     setupTooltip(lg);
     setupTooltip(hs);
 
-    assignData(eg);
-    assignData(lg);
-    assignData(hs);
-
-    $('#example').dataTable({
+    var table = $('#example').DataTable({
         serverSide: true,
         ajax: {
             url: "http://bic-hp-z400/index.php?r=gene/index",
             method: "get",
-            "dataSrc": "data"
+            dataSrc: "data"
         },
         columns: [
             { data: 'agi' },
@@ -62,7 +58,10 @@ function loadExperiment() {
         scroller: {
             loadingIndicator: true
         }
-    } ).columnFilter( {
+    });
+
+    // Take note of the lowercase dataTable, this is the old API
+    $("#example").dataTable().columnFilter( {
         sPlaceHolder: "head:after",
         aoColumns: [
             { type: 'text' },
@@ -70,21 +69,89 @@ function loadExperiment() {
             { type: 'number-range' },
             { type: 'number-range' }
         ]
-    }).on( 'search.dt', function () {
+    });
+    $('#example').on( 'search.dt', function () {
         $(".filter_column input").removeClass('filtered');
         $(".filter_column input").each(function() {
             if( $(this).val() != '' ) {
                 $(this).addClass('filtered');
             }
         });
+        return true;
+    });
+
+    $('tbody').on( 'click', 'tr', function () {
+        $("tbody tr.selected").removeClass("selected");
+        $(this).addClass("selected");
+        var data = table.row(this).data();
+        loadINTACT(data);
     } );
 }
 
-function colorCellTypes(ele, color) {
+function colorCellTypes(ele, color, data = []) {
+    var j;
+
     $.each(tissues, function(i, tissue) {
-        ele.select('#' + tissue).transition().duration(1000).attr('fill',color(i))
+
+        if( data.length > 0 ) {
+            j = data[i];
+        } else {
+            j = i;
+        }
+
+        ele.select('#' + tissue).transition().duration(1000).attr('fill',color(j))
+    });
+}
+
+function loadINTACT(data) {
+    var color = d3.scale.linear()
+        .domain([0, 100, 1000])
+        .range(["white", "green", "red"]);
+
+    var int17_eg = ['suspensor'];
+    var iqd15_eg = ['vascular', 'vascular-initials'];
+    var rps5a_eg = ['ground-initials', 'ground', 'inner-upper','protoderm', 'hypophysis', 'qc','columella'];
+
+    var iqd15_lg = ['vascular', 'vascular-initials'];
+    var rps5a_lg = ['suspensor', 'ground-initials', 'ground', 'inner-upper','protoderm', 'hypophysis', 'qc','columella'];
+
+    var wox5_hs = ['qc'];
+
+    var dataEG = [], dataLG = [], dataHS = [];
+    $.each(tissues, function(i, tissue) {
+        if( int17_eg.indexOf(tissue) > -1 ) {
+            dataEG[i] = data.int17_eg;
+        }
+        if( iqd15_eg.indexOf(tissue) > -1 ) {
+            dataEG[i] = data.iqd15_eg;
+        }
+        if( rps5a_eg.indexOf(tissue) > -1 ) {
+            dataEG[i] = data.rps5a_eg;
+        }
+
+        if( iqd15_lg.indexOf(tissue) > -1 ) {
+            dataLG[i] = data.iqd15_lg;
+        }
+        if( rps5a_lg.indexOf(tissue) > -1 ) {
+            dataLG[i] = data.rps5a_lg;
+        }
+
+        if( wox5_hs.indexOf(tissue) > -1 ) {
+            dataHS[i] = data.wox5_hs;
+        } else {
+            dataHS[i] = 0;
+        }
     });
 
+    var eg = d3.select('#eg'), lg = d3.select('#lg'), hs = d3.select('#hs');
+
+    colorCellTypes(lg, color, dataLG);
+    colorCellTypes(eg, color, dataEG);
+    colorCellTypes(hs, color, dataHS);
+
+    assignData(lg, dataLG);
+    assignData(eg, dataEG);
+    assignData(hs, dataHS);
 }
 
 function retrieveFillColor(ele) {
@@ -105,7 +172,11 @@ function setupTooltip(ele) {
         .direction('e')
         .offset([0, 20])
         .html( function(d) {
-            return d.tissue + "<br /> Expression value: " + d.value;
+            if ( d ) {
+                return d.tissue + "<br /> Expression value: " + d.value;
+            } else {
+                return "N/A";
+            }
         } );
 
     ele.call(tip);
@@ -124,11 +195,11 @@ function setupTooltip(ele) {
     });
 }
 
-function assignData(ele) {
+function assignData(ele, data) {
 
     $.each(tissues, function(i, tissue) {
         ele.select('#' + tissue)
-            .data([{'tissue' : tissue, 'value' : Math.round(Math.random() * 100)}])
+            .data([{'tissue' : tissue, 'value' : data[i]}])
     });
 }
 
