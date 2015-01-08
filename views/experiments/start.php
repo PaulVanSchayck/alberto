0552aece564f60adab373da724b8971a0efad9b5
PaulVanSchayck <polleke@gmail.com>
2015-01-08 16:57:51 +0100

    Show available experiments on start page

--- views/experiments/start.php
+++ views/experiments/start.php
@@ -7,3 +7,26 @@
 </p>
 
 <h3>Available Experiments</h3>
+
+<div class="container experiments">
+    <div class="row">
+        <div class="col-lg-2 experiment" data-toggle="tooltip" data-original-title="Transcriptome of individual cell types">
+            <a href="/index.php#nav-intact" onclick="navInfo.setExperiment('intact')">
+                <img src="images/start/intact.jpg">
+                <span>Cell type-specific</span>
+            </a>
+        </div>
+
+        <div class="col-lg-2 experiment" data-toggle="tooltip" data-original-title="FACS gradient of root">
+            <a href="/index.php#nav-rootgradient" onclick="navInfo.setExperiment('rootgradient')">
+                <img src="images/start/root.jpg">
+                <span>Root gradients</span>
+            </a>
+        </div>
+
+    </div>
+</div>
+
+<script type="application/javascript">
+    $("#start div.experiment").tooltip({'placement': 'bottom'});
+</script>
