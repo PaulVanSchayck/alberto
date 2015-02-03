0254086ce4490463ebfc6a1067e984c3842507a8
PaulVanSchayck <polleke@gmail.com>
2015-02-03 14:40:09 +0100

    Remove relative gene code

--- models/Intact.php
+++ models/Intact.php
@@ -7,45 +7,9 @@ use yii\db\ActiveRecord;
 
 class Intact extends ActiveRecord {
 
-    private $relativeGene;
-
     public static function tableName()
     {
         return 'intact';
-    }
-
-    public function getRelativeFields()
-    {
-        $intact = Yii::$app->params['experiments']['intact'];
-
-        $fields = [];
-
-        foreach ( $intact['columns'] as $column ) {
-            if ( $column['type'] == 'abs' ) {
-                $fields[] = $column['field'];
-            }
-        }
-
-        return [];
-    }
-
-    public function setRelativeGene($gene)
-    {
-        $this->relativeGene = $gene;
-    }
-
-    public function getRelativeField($name)
-    {
-        return $this->$name;
-    }
-
-    public function __get($name)
-    {
-        if ( array_search($name, $this->getRelativeFields()) ) {
-            return $this->getRelativeField($name);
-        } else {
-            return parent::__get($name);
-        }
     }
 
     public function getGene()
