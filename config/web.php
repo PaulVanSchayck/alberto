1120ac16f62ea2041fdcafba62d44865e116a4ab
Paul van Schayck <polleke@gmail.com>
2015-03-04 12:29:25 +0100

    Update composer, remove unused packages

--- config/web.php
+++ config/web.php
@@ -73,9 +73,6 @@ if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = 'yii\debug\Module';
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
 }
 
 return $config;
