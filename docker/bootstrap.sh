2e6e7e4d11d40938caddd1d4de7ac81f0ff995a5
Paul van Schayck <polleke@gmail.com>
2020-11-19 21:33:07 +0100

    Add base Docker install

---
+++ docker/bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+while ! mysqladmin ping -h db --silent; do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+
+cd /opt/app || exit
+
+# Install composer dependencies
+composer install
+
+# Run yii2 deployment tool
+./deployment/init --env=dev --overwrite=yes
+
+if [[ $? -gt 0  ]]; then
+    echo "Yii2 deployment tool failed. Check for errors."
+    exit 1;
+fi
+
+# Install DB if does not exist yet
+mysql -h db -u root -e 'use alberto' > /dev/null 2>&1
+if [[ $? -gt 0  ]]; then
+    # Create
+    mysql -h db -u root -e "CREATE SCHEMA IF NOT EXISTS alberto DEFAULT CHARACTER SET utf8 ;"
+
+    # Load base database
+    mysql -h db -D alberto < /opt/annotation_create_import.sql
+    mysql -h db -D alberto < /opt/intact.sql
+
+fi
+
+# Start Apache2 in background
+apache2ctl start
+
+# Watch the yii2 log
+exec tail -f --retry /opt/app/runtime/logs/app.log
